46e5018922d7d4d1c6ac6e392e5ebba2c426d534
saeedkokash <saeedkokash@gmail.com>
2022-12-09 13:24:28 +0200

    added logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import logo from './assets/logo.png';
 
 function App() {
   return (
     <div className="App">
       Hello Bid-Hammers!
+      <img src={logo} alt="logo" />
     </div>
   );
 }
