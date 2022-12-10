dfda3fd7f8c9099da613e81c5627d16dae630167
saeedkokash <saeedkokash@gmail.com>
2022-12-10 16:41:43 +0200

    craeted redux and init state, signup, signin components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import logo from './assets/logo.png';
+import Login from './components/auth/Login';
+
+import Signup from './components/auth/Signup';
 
 function App() {
   return (
     <div className="App">
       Hello Bid-Hammers!
       <img src={logo} alt="logo" />
+
+      <Login />
+
+      <Signup />
+
+
     </div>
   );
 }
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,40 @@
 import React from 'react'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { login } from '../../store/actions/authActions'
+
 function Login() {
+
+  const dispatch = useDispatch();
+
+  const error = useSelector(state => state.auth.error);
+  const loading = useSelector(state => state.auth.loading);
+
   return (
-    <div>Login</div>
+    <div>
+
+      <h1>Login</h1>
+
+      <form onSubmit={(e) => login(dispatch, e)}>
+
+        <label>Email</label>
+        <input type="email" name="email" placeholder='email' autoComplete='email' required/>
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder='password' autoComplete='current-password' required/>
+
+        {error && <p>{error}</p>}
+        <p>{loading ? 'Loading...' : ''}</p>
+
+        <button type="submit">Login</button>
+
+        <p>Not Registered?
+          <a href="/signup">Signup</a>
+        </p>
+
+      </form>
+      
+    </div>
   )
 }
 
