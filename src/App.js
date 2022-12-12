08060d27bb3215b422b991a56a1495c1f3fe7b05
ahmadjubran <ahmadjubran.98@hotmail.com>
2022-12-12 23:07:01 +0000

    Filter for items

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 
 import Categories from "./components/Categories";
-import ListOfItems from "./components/Categories/ListOfItems";
+import Items from "./components/Categories/Items";
 import NavBar from "./components/header/NavBar";
 import Hero from "./components/hero/Hero";
 import Profile from "./components/Profile";
@@ -13,39 +13,29 @@ import Profile from "./components/Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   // const isAuth = useSelector((state) => state.auth.isAuth);
 
   return (
     <BrowserRouter>
-
       <header className="header">
         <NavBar />
       </header>
 
-       
-
-
-
       <Routes>
-
-      {/* <div className="profile-container"> */}
-        <Route exact path="/" element={<Hero />} />
+        {/* <div className="profile-container"> */}
+        <Route path="/" element={<Hero />} />
         {/* <Hero /> */}
-        <Route exact path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
         {/* <Profile /> */}
         {/* <Route exact path="/categories" element={<Categories />} />
         <Route exact path="/categories/item" element={<ListOfItems />} /> */}
-      {/* </div> */}
+        {/* </div> */}
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/items" element={<Items />} />
 
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
-
-      <Categories /> 
-      <ListOfItems />
-
     </BrowserRouter>
   );
 }
